723de4a89e2591912d5c7729865c7a0fad528ee9
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2025-12-20 15:22:46 +0100

    refac: move all toplevel logic into `main`

--- src/unit.ts
+++ src/unit.ts
@@ -11,8 +11,11 @@ import { parseGrammarTestCase, runGrammarTestCase } from './unit/index.ts'
 import type { GrammarTestCase } from './unit/model.ts'
 import { createReporter } from './unit/reporter.ts'
 
-function collectGrammarOpts(value: string, previous: string[]): string[] {
-	return previous.concat([value])
+const MAX_CONCURRENT_TESTS = 8
+
+enum ExitCode {
+	Success = 0,
+	Failure = 1,
 }
 
 interface CliOptions {
@@ -23,89 +26,93 @@ interface CliOptions {
 	xunitFormat: 'generic' | 'gitlab'
 }
 
-program
-	.description('Run Textmate grammar test cases using vscode-textmate')
-	.option(
-		'-g, --grammar <grammar>',
-		"Path to a grammar file. Multiple options supported. 'scopeName' is taken from the grammar",
-		collectGrammarOpts,
-		[],
-	)
-	.option(
-		'--config <configuration.json>',
-		'Path to the language configuration, package.json by default',
-	)
-	.option(
-		'-c, --compact',
-		'Display output in the compact format, which is easier to use with VSCode problem matchers',
-	)
-	.option(
-		'--xunit-report <report.xml>',
-		'Path to directory where test reports in the XUnit format will be emitted in addition to console output',
-	)
-	.option(
-		'--xunit-format <generic|gitlab>',
-		'Format of XML reports generated when --xunit-report is used. `gitlab` format is suitable for viewing the results in GitLab CI/CD web GUI',
-	)
-	.version(VERSION)
-	.argument(
-		'<testcases...>',
-		'A glob pattern(s) which specifies testcases to run, e.g. "./tests/**/test*.dhall". Quotes are important!',
-	)
-	.parse(process.argv)
-
-const options = program.opts<CliOptions>()
-
-const MAX_CONCURRENT_TESTS = 8
-
-enum ExitCode {
-	Success = 0,
-	Failure = 1,
+function collectGrammarOpts(value: string, previous: string[]): string[] {
+	return previous.concat([value])
 }
 
-const { grammars } = loadConfiguration(options.config, undefined, options.grammar)
-const registry = createRegistry(grammars)
+async function main(): Promise<ExitCode> {
+	program
+		.description('Run Textmate grammar test cases using vscode-textmate')
+		.option(
+			'-g, --grammar <grammar>',
+			"Path to a grammar file. Multiple options supported. 'scopeName' is taken from the grammar",
+			collectGrammarOpts,
+			[],
+		)
+		.option(
+			'--config <configuration.json>',
+			'Path to the language configuration, package.json by default',
+		)
+		.option(
+			'-c, --compact',
+			'Display output in the compact format, which is easier to use with VSCode problem matchers',
+		)
+		.option(
+			'--xunit-report <report.xml>',
+			'Path to directory where test reports in the XUnit format will be emitted in addition to console output',
+		)
+		.option(
+			'--xunit-format <generic|gitlab>',
+			'Format of XML reports generated when --xunit-report is used. `gitlab` format is suitable for viewing the results in GitLab CI/CD web GUI',
+		)
+		.version(VERSION)
+		.argument(
+			'<testcases...>',
+			'A glob pattern(s) which specifies testcases to run, e.g. "./tests/**/test*.dhall". Quotes are important!',
+		)
+		.parse(process.argv)
 
-const reporter = createReporter(options.compact, options.xunitFormat, options.xunitReport)
+	const options = program.opts<CliOptions>()
 
-const rawTestCases = program.args.flatMap((x) => globSync(x))
+	const { grammars } = loadConfiguration(options.config, undefined, options.grammar)
+	const registry = createRegistry(grammars)
 
-// Early exit if no test cases found
-if (rawTestCases.length === 0) {
-	console.log(chalk.red('ERROR') + ' no test cases found')
-	process.exit(ExitCode.Failure)
+	const reporter = createReporter(options.compact, options.xunitFormat, options.xunitReport)
+
+	const rawTestCases = program.args.flatMap((x) => globSync(x))
+
+	// Early exit if no test cases found
+	if (rawTestCases.length === 0) {
+		console.log(chalk.red('ERROR') + ' no test cases found')
+		process.exit(ExitCode.Failure)
+	}
+
+	const limit = pLimit(MAX_CONCURRENT_TESTS)
+
+	// Run tests in parallel
+	const testResults: Promise<number[]> = Promise.all(
+		rawTestCases.map(async (filename): Promise<number> => {
+			let testCase: GrammarTestCase
+
+			// Read and parse test case
+			try {
+				testCase = parseGrammarTestCase(fs.readFileSync(filename, 'utf8'))
+			} catch (error) {
+				reporter.reportParseError(filename, error)
+				return ExitCode.Failure
+			}
+
+			// Execute test case
+			try {
+				const failures = await limit(() => runGrammarTestCase(registry, testCase))
+				reporter.reportTestResult(filename, testCase, failures)
+				return failures.length === 0 ? ExitCode.Success : ExitCode.Failure
+			} catch (error: any) {
+				reporter.reportGrammarTestError(filename, testCase, error)
+				return ExitCode.Failure
+			}
+		}),
+	)
+
+	let success = false
+	testResults.then((xs) => {
+		reporter.reportSuiteResult()
+		success = xs.every((x) => x === ExitCode.Success)
+	})
+
+	return success ? ExitCode.Success : ExitCode.Failure
 }
 
-const limit = pLimit(MAX_CONCURRENT_TESTS)
-
-// Run tests in parallel
-const testResults: Promise<number[]> = Promise.all(
-	rawTestCases.map(async (filename): Promise<number> => {
-		let testCase: GrammarTestCase
-
-		// Read and parse test case
-		try {
-			testCase = parseGrammarTestCase(fs.readFileSync(filename, 'utf8'))
-		} catch (error) {
-			reporter.reportParseError(filename, error)
-			return ExitCode.Failure
-		}
-
-		// Execute test case
-		try {
-			const failures = await limit(() => runGrammarTestCase(registry, testCase))
-			reporter.reportTestResult(filename, testCase, failures)
-			return failures.length === 0 ? ExitCode.Success : ExitCode.Failure
-		} catch (error: any) {
-			reporter.reportGrammarTestError(filename, testCase, error)
-			return ExitCode.Failure
-		}
-	}),
-)
-
-// Process results
-testResults.then((xs) => {
-	reporter.reportSuiteResult()
-	const success = xs.every((x) => x === ExitCode.Success)
-	process.exit(success ? ExitCode.Success : ExitCode.Failure)
+main().then((code) => {
+	process.exit(code)
 })
