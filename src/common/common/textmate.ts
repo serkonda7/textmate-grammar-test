b0b5a3cb6225f3efbf4e8a4793bd6cccbe2322f6
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2026-01-11 17:27:59 +0100

    further code refactor

--- src/common/common/textmate.ts
+++ src/common/common/textmate.ts
@@ -16,7 +16,9 @@ export function createRegistry(gs: IGrammarConfig[]): tm.Registry {
 }
 
 // TODO consider rename to createTmRegistry
-function createRegistryFromGrammars(grammars: Array<{ grammar: IGrammarConfig; content: string }>) {
+function createRegistryFromGrammars(
+	grammars: Array<{ grammar: IGrammarConfig; content: string }>,
+): tm.Registry {
 	const grammar_map = new Map<string, tm.IRawGrammar>()
 	const injection_map = new Map<string, string[]>()
 
@@ -24,39 +26,49 @@ function createRegistryFromGrammars(grammars: Array<{ grammar: IGrammarConfig; c
 		const raw = tm.parseRawGrammar(content, grammar.path)
 		grammar_map.set(grammar.scopeName || raw.scopeName, raw)
 
-		if (grammar.injectTo) {
-			for (const inject_scope of grammar.injectTo) {
-				let injections = injection_map.get(inject_scope)
-				if (!injections) {
-					injections = []
-					injection_map.set(inject_scope, injections)
-				}
-				injections.push(grammar.scopeName)
+		if (!grammar.injectTo) {
+			continue
+		}
+
+		// Add injections
+		for (const inject_scope of grammar.injectTo) {
+			let injections = injection_map.get(inject_scope)
+			if (!injections) {
+				injections = []
+				injection_map.set(inject_scope, injections)
 			}
+			injections.push(grammar.scopeName)
 		}
 	}
 
-	const oniguruma_lib = createOnigurumaLib()
+	const onigLib = createOnigurumaLib()
 
 	return new tm.Registry({
-		onigLib: oniguruma_lib,
-		loadGrammar: (scopeName) => {
-			if (grammar_map.get(scopeName) !== undefined) {
-				return new Promise((fulfill) => {
-					fulfill(grammar_map.get(scopeName))
-				})
+		onigLib,
+
+		async loadGrammar(scopeName) {
+			const grammar = grammar_map.get(scopeName)
+			if (!grammar) {
+				console.warn(`grammar not found for "${scopeName}"`)
+				return null
 			}
-			console.warn(`grammar not found for "${scopeName}"`)
-			return null
+			return grammar
 		},
+
 		getInjections: (scopeName) => {
-			const xs = scopeName.split('.')
-			let injections: string[] = []
-			for (let i = 1; i <= xs.length; i++) {
-				const subScopeName = xs.slice(0, i).join('.')
-				injections = [...injections, ...(injection_map.get(subScopeName) || [])]
+			const parts = scopeName.split('.')
+			const injections: string[] = []
+			let cur_scope = ''
+
+			for (const part of parts) {
+				// Build current sub-scope
+				cur_scope = cur_scope ? `${cur_scope}.${part}` : part
+
+				// Add injections if any
+				injections.push(...(injection_map.get(cur_scope) ?? []))
 			}
+
 			return injections
 		},
-	} as tm.RegistryOptions)
+	})
 }
