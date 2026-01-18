b2499a2cc8930543c34f658ea9a56a90ffc3226a
Lukas Neubert <40118727+serkonda7@users.noreply.github.com>
2026-01-18 16:35:07 +0100

    lint

--- test/unit/test_runner.test.ts
+++ test/unit/test_runner.test.ts
@@ -10,16 +10,15 @@ test('OK on correct grammar test', async () => {
 	expect(res.failures).toHaveLength(0)
 })
 
-
 test('report missing scopes', async () => {
 	const res = unwrap(await runner.test_file(read_data('missing.testlang')))
-	expect(res.failures[0].missing).toEqual(["string.xy", "variable.interpolation.xy"])
+	expect(res.failures[0].missing).toEqual(['string.xy', 'variable.interpolation.xy'])
 })
 
 test('report unexpected scopes', async () => {
 	const res = unwrap(await runner.test_file(read_data('unexpected.testlang')))
 	expect(res.failures).toHaveLength(2)
-	expect(res.failures[0].unexpected).toEqual(["source.xy"])
+	expect(res.failures[0].unexpected).toEqual(['source.xy'])
 })
 
 test('line end', async () => {
